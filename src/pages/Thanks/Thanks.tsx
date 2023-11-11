2b85a7373c1c5e93c491dab3b3ff87dbfb8f2dc0
Ilya Kamovich <kamovichilya@gmail.com>
2023-11-11 16:19:07 +0300

    pix

--- src/pages/Thanks/Thanks.tsx
+++ src/pages/Thanks/Thanks.tsx
@@ -4,7 +4,6 @@ import './Thanks.styles.scss';
 import { thanksName, thanksPhone } from '../../store/thanks/thanks.selectors';
 import { useTypeSelector } from '../../hooks/useTypeSelector';
 import { Fragment } from 'react';
-import { Helmet } from 'react-helmet';
 
 const Thanks = () => {
   const name = useTypeSelector(thanksName);
@@ -12,34 +11,6 @@ const Thanks = () => {
 
   return (
     <Fragment>
-      <Helmet>
-        <script type="text/javascript">
-          {`(function (m, e, t, r, i, k, a) {
-        m[i] =
-          m[i] ||
-          function () {
-            (m[i].a = m[i].a || []).push(arguments);
-          };
-        m[i].l = 1 * new Date();
-        for (var j = 0; j < document.scripts.length; j++) {
-          if (document.scripts[j].src === r) {
-            return;
-          }
-        }
-        (k = e.createElement(t)), (a = e.getElementsByTagName(t)[0]), (k.async = 1), (k.src = r), a.parentNode.insertBefore(k, a);
-      })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
-
-      ym(95423799, 'init', {
-        clickmap: true,
-        trackLinks: true,
-        accurateTrackBounce: true,
-      });`}
-        </script>
-        <noscript>
-          {`<div><img src="https://mc.yandex.ru/watch/95423799" style="position: absolute; left: -9999px" alt="" /></div
-    >`}
-        </noscript>
-      </Helmet>
       <div className="thanks">
         <div className="container">
           <header className="thanks__header">
